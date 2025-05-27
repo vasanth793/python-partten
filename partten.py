a047d139995fd389cb639d2d84314962dbb5333f
vasanth793 <vasanthakumar0790@gmail.com>
2025-05-27 16:19:41 +0530

    py

---
+++ partten.py
@@ -0,0 +1,19 @@
+# rows=(int(input("numaber:")))
+# for i in range (1,rows+1):
+#     for j in range (1,i+1):
+#         print("*", end=" ")
+#     print()    
+
+rows=(int(input("numaber:")))
+# for i in range (rows,0, -1):
+#     for j in range (1,i + 1):
+#         print( i,end="")
+#     print()    
+
+# for i in range (1,rows -1):
+#     for j in range (rows -i):
+#         print(" ",end=" ")
+#     for k in range(1,2*i):
+#         print(i, end=" " )
+#     print()     
+
