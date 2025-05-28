2a2a8321fd7081c2dc6b69a7fcf00f92c37b0e8b
vasanth793 <vasanthakumar0790@gmail.com>
2025-05-28 15:31:28 +0530

    add

--- partten.py
+++ partten.py
@@ -4,7 +4,7 @@
 #         print("*", end=" ")
 #     print()    
 
-rows=(int(input("numaber:")))
+# rows=(int(input("numaber:")))
 # for i in range (rows,0, -1):
 #     for j in range (1,i + 1):
 #         print( i,end="")
@@ -16,4 +16,98 @@ rows=(int(input("numaber:")))
 #     for k in range(1,2*i):
 #         print(i, end=" " )
 #     print()     
+
+
+
+
+# for i in range (1,rows +1):
+#     for j in range(1,i+1):
+#         print(i,end=" ")
+#     print()    
+
+
+
+# for v in range(rows ,0, -1):
+#     for k in range(1 ,v +1):
+#         print(k , end="")
+#     print()    
+
+
+# works !!! 1
+
+# # inp = (int(input("enter your number : ")))
+
+# lst=[]
+# for i in range (inp+1):
+#     lst.append(i)
+# print(lst)
+
+
+# works !!! 2
+
+# inp = (int(input("enter your number : ")))
+# print(inp)
+# int = (input("enter your value : "))
+# sec=[int]
+# print(sec)
+
+# sec 2 
+
+# inp = (int(input("enter your number : ")))
+# print(inp)
+# int = (input("enter your value : "))
+# sec=[int]
+# print(sec)
+
+
+# infi
+
+# n=int(input("enter vqlue:"))
+
+
+# k=0
+# while (k < n):
+#     inp=int(input("enter nqmber:"))
+#     lst=[]
+#     for i in range (inp+1):
+#         lst.append(i)
+#     print(lst) 
+#     k+=1
+
+
+# example
+# v=int(input("enter number :"))
+
+# k=0
+
+# while (k<v):
+#     n=int(input("enter value :"))
+#     let=[]
+#     for i in range(n+1):
+#         let.append(i)
+#     print(let) 
+#     k+=1   
+
+#odd even
+# n=int(input("enter your number:"))
+
+# iput=[]
+# odd=[]
+# even=[]
+# for i in range (1,n):
+#     iput.append(i)
+#     if (i % 2 ==0):
+#         even.append(i)
+#     else:
+#         odd.append(i)
+
+# print(odd)
+# print(even)        
+
+# user=int(input("choice odd or even .( odd means 1 ,even means 2 ) type yours:"))
+# if (user>1):
+#     print(even)
+# else:
+#     print(odd)
+
 
